45ca57e1ce30e839f52609316bfb0ac8ac07b5d0
Christopher Larson <christopher.larson@revature.net>
2021-04-07 20:02:53 -0400

    uncommented haikugenerator

--- HaikuWriter/BusinessLogic/HaikuGenerator.cs
+++ HaikuWriter/BusinessLogic/HaikuGenerator.cs
@@ -2,7 +2,7 @@ namespace BusinessLogic
 {
     public class HaikuGenerator
     {
-        /*Repository _repo = new Repository;
+        Repository _repo = new Repository;
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }
@@ -12,6 +12,6 @@ namespace BusinessLogic
             Line2 = _repo.GetHaiku7();
             Line3 = _repo.GetHaiku5(Line1);
         }
-        */
+        
     }
 }

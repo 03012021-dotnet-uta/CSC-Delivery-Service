9f3fb30d723625b39dccd406ec8f2f74ba9ba615
sagarluitel01 <sagar.luitel@revature.net>
2021-04-08 08:44:30 -0600

    committing

--- HaikuWriter/BusinessLogic/HaikuGenerator.cs
+++ HaikuWriter/BusinessLogic/HaikuGenerator.cs
@@ -4,19 +4,7 @@ namespace BusinessLogic
 {
     public class HaikuGenerator
     {
-<<<<<<< HEAD
-        private readonly HaikuRepo _repo;
-         
-        private readonly UserRepo _repoUser;
-
-        public HaikuGenerator(HaikuRepo repo, UserRepo repoUser){
-            this._repo = repo;
-            this._repoUser = repoUser;
-
-        }
-=======
         Repository _repo = new Repository;
->>>>>>> 45ca57e1ce30e839f52609316bfb0ac8ac07b5d0
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }

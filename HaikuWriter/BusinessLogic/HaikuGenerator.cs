0be7810e11eb6dd48823ebbc3e9bff46abb45910
sagarluitel01 <sagar.luitel@revature.net>
2021-04-08 08:51:36 -0600

    Updating HaikuGenerator

--- HaikuWriter/BusinessLogic/HaikuGenerator.cs
+++ HaikuWriter/BusinessLogic/HaikuGenerator.cs
@@ -4,17 +4,15 @@ namespace BusinessLogic
 {
     public class HaikuGenerator
     {
-<<<<<<< HEAD
-        Repository _repo = new Repository;
-=======
         private readonly HaikuRepo _repo;
+        private readonly UserRepo _userRepo;
 
-        public HaikuGenerator(HaikuRepo repo)
+        public HaikuGenerator(HaikuRepo repo, UserRepo userrepo)
         {
             _repo = repo;
+            this._userRepo = _userrepo;
         }
 
->>>>>>> e488a20a9f1107f486ead522c20c66a6c43a774d
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }

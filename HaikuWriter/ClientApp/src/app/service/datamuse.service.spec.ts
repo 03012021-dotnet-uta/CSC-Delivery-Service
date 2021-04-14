96b57af7b32d7a896a7860934bb5b940ffdf4c31
Catherine Cheatle <catherine.cheatle@revature.net>
2021-04-14 16:52:09 -0400

    ang test for pipeline

--- HaikuWriter/ClientApp/src/app/service/datamuse.service.spec.ts
+++ HaikuWriter/ClientApp/src/app/service/datamuse.service.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient, HttpEvent, HttpEventType } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 import { DatamuseService } from './datamuse.service';
 
-// describe('DatamuseService', () => {
-//   let service: DatamuseService;
+describe('DatamuseService', () => {
+    let service: DatamuseService;
+    let words: any;
+    let httpMock: HttpTestingController;
+    let httpClient: HttpClient;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(DatamuseService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DatamuseService]
+    });
+    service = TestBed.inject(DatamuseService);
+    httpMock = TestBed.get(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+      expect(service).toBeTruthy();
+  });
+
+  it('getdata() should get words from url', async() => {
+    let url = 'https://api.datamuse.com/words?rel_syn=ocean';
+    words = {word: "sea", score: 4730};
+    service.getData(url).subscribe((word) => {
+        expect(word).toEqual(words);
+    });
+
+    const req = httpMock.expectOne(url);
+
+    httpMock.verify();
+  })
+});
